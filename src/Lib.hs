6acf61c54bd8ee5b8c017535e2bdcd693dc303b5
Travis <tssund93@gmail.com>
2021-01-03 14:45:37 -0600

    added some documentation

--- src/Lib.hs
+++ src/Lib.hs
@@ -18,32 +18,46 @@ import SDL (($=), (^*), (^/))
 import qualified SDL
 import System.Exit (exitSuccess)
 
+-- | global SDL renderer
 newtype GRenderer = GRenderer SDL.Renderer
+
 instance Semigroup GRenderer where (<>) = const id
 instance Monoid GRenderer where mempty = undefined
 instance Component GRenderer where type Storage GRenderer = A.Global GRenderer
 
+-- | global SDL window
 newtype GWindow = GWindow SDL.Window
+
 instance Semigroup GWindow where (<>) = const id
 instance Monoid GWindow where mempty = undefined
 instance Component GWindow where type Storage GWindow = A.Global GWindow
 
+-- | a datatype containing all available sprites in sprite bank
 data SpriteType = SpritePlayer
     deriving (Eq, Ord)
 
+-- | global sprite bank for accessing sprite textures
 newtype GSpriteBank = GSpriteBank (Map SpriteType SDL.Texture)
+
 instance Semigroup GSpriteBank where (<>) = (<>)
 instance Monoid GSpriteBank where mempty = mempty
 instance Component GSpriteBank where type Storage GSpriteBank = A.Global GSpriteBank
 
+-- | countdown until move animations are done and player has control again
 newtype GMoveTime = GMoveTime Double
+
 instance Semigroup GMoveTime where (<>) = (<>)
 instance Monoid GMoveTime where mempty = mempty
 instance Component GMoveTime where type Storage GMoveTime = A.Global GMoveTime
 
+-- | marks the player entity
 data Player = Player deriving (Show)
+
 instance A.Component Player where type Storage Player = A.Unique Player
 
+{- | current position, stores both actual position on the grid and
+rendered position for smoothly transitioning from one grid position to another
+-}
 data Position
     = Position
         (SDL.V2 Int)
@@ -51,9 +65,12 @@ data Position
         (SDL.V2 Double)
         -- ^ display position
     deriving (Show)
+
 instance A.Component Position where type Storage Position = A.Map Position
 
+-- | entity's velocity
 newtype Velocity = Velocity (SDL.V2 Int) deriving (Show)
+
 instance A.Component Velocity where type Storage Velocity = A.Map Velocity
 
 data MovingUp = MovingUp deriving (Show)
@@ -65,10 +82,16 @@ instance Component MovingDown where type Storage MovingDown = A.Unique MovingDow
 data MovingLeft = MovingLeft deriving (Show)
 instance Component MovingLeft where type Storage MovingLeft = A.Unique MovingLeft
 
+-- | sprite used to display entity
 newtype Sprite = Sprite SpriteType
+
 instance Component Sprite where type Storage Sprite = A.Map Sprite
 
+{- | entity size, scaled to grid. so a size of (V2 1 1) will take up
+ one cell on the grid
+-}
 newtype Size = Size (SDL.V2 Int) deriving (Show)
+
 instance Component Size where type Storage Size = A.Map Size
 
 A.makeWorld
@@ -111,23 +134,23 @@ initialize = do
     SDL.initializeAll
 
     window <-
-        liftIO $
-            SDL.createWindow
-                "My SDL Application"
-                SDL.defaultWindow{SDL.windowInitialSize = SDL.V2 480 360}
-    renderer <- liftIO $ SDL.createRenderer window (-1) SDL.defaultRenderer
+        SDL.createWindow
+            "My SDL Application"
+            SDL.defaultWindow{SDL.windowInitialSize = SDL.V2 480 360}
+    renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
     _windowEty <- A.newEntity (GWindow window)
     _rendererEty <- A.newEntity (GRenderer renderer)
     _moveTimeEty <- A.newEntity (GMoveTime 0)
 
-    playerSprite <- liftIO $ SDL.loadBMP "media/megumin.bmp"
-    playerTexture <- liftIO $ SDL.createTextureFromSurface renderer playerSprite
+    playerSprite <- SDL.loadBMP "media/megumin.bmp"
+    playerTexture <- SDL.createTextureFromSurface renderer playerSprite
     _spriteBankEty <- A.newEntity (GSpriteBank $ Map.fromList [(SpritePlayer, playerTexture)])
 
     _player <- makePlayer
 
     return ()
 
+-- | how fast transition between positions should be, smaller number is faster
 turnTime :: Double
 turnTime = 0.125
 
@@ -189,6 +212,7 @@ stepPlayerMovement = do
     A.cmap $ \(Player, MovingDown, Velocity (SDL.V2 x y)) ->
         Velocity (SDL.V2 x (y + playerSpeed))
 
+-- how big each cell on the grid is in pixels
 cellSize :: SDL.V2 Int
 cellSize = 32
 
